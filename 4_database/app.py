3a96166a8b328305a184e71e78c665a221b649d1
sty945 <sutaoyu@yeah.net>
2019-11-20 16:00:57 +0800

    update 数据库初始化

--- 4_database/app.py
+++ 4_database/app.py
@@ -7,3 +7,32 @@
 # Author: sty
 
 # File: app.py
+
+
+import os
+import sys
+
+import click
+from flask import Flask
+from flask import redirect, url_for, abort, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, 'data.db'))
+
+db = SQLAlchemy(app)
