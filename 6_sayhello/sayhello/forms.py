716d4209298bc35f58c3faab5b245e214a3de377
sty945 <sutaoyu@yeah.net>
2019-11-28 16:27:17 +0800

    编写表单类

---
+++ 6_sayhello/sayhello/forms.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# !/usr/bin/env python
+
+# Time: 2019/11/28 15:42
+
+# Author: sty
+
+# File: forms.py
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+
+class HelloForm(FlaskForm):
+    """
+    问候表单
+    """
+    name = StringField('name', validators=[DataRequired(), Length(1, 20)])
+    body = TextAreaField('Message', validators=[DataRequired(), Length(1, 200)])
+    submit = SubmitField()
+
