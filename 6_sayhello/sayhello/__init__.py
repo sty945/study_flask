057dd738a79c410e04360dbec82b5641f5ac8583
sty945 <sutaoyu@yeah.net>
2019-11-28 15:38:59 +0800

    修改sayhello的初始化代码

--- 6_sayhello/sayhello/__init__.py
+++ 6_sayhello/sayhello/__init__.py
@@ -7,3 +7,13 @@
 # Author: sty
 
 # File: __init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask('sayhello')
+app.config.from_pyfile('settings.py')
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+db = SQLAlchemy(app)
--- 6_sayhello/sayhello/models.py
+++ 6_sayhello/sayhello/models.py
@@ -7,3 +7,16 @@
 # Author: sty
 
 # File: models.py
+
+
+from datetime import datetime
+
+from . import db
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    body = db.Column(db.String(200))
+    # timestamp用于存储留言发表时间戳，采用默认的当前时间
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
